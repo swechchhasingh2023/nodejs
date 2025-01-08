c3cb431a715986ce2a2eee07f949e917ca69af12
Swechchha Singh <swechchhasingh2023@gmail.com>
2025-01-08 13:36:56 +0530

    code update

---
+++ 5-Javascript-fundamentals-in-node/index.js
@@ -0,0 +1,31 @@
+const app = require("./app")
+/*
+var x = 20
+if(x==20){
+    console.log("matched")
+}
+*/
+
+/*
+var x = 20;
+for (let i = 0; i <=10; i++){
+    console.log(i);
+}
+*/
+
+/*
+var arr = [2,3,4,5,6];
+// console.log(arr);
+console.log(arr[0]);
+*/
+/*
+console.log(app)
+console.log(app.x)
+console.log(app.y)
+console.log(app.z())
+*/
+
+const arr = [2,3,4,5,6,8,6,0]
+arr.filter((item)=>{
+    console.log(item)
+})
